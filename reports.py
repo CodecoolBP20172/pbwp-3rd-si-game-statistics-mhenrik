65439e5d649b842e86f0969e55539c80645e8f42
Henrik Miszoglad <henrik.miszoglad@gmail.com>
2017-06-24 12:19:43 +0200

    file reader added

--- reports.py
+++ reports.py
@@ -1,2 +1,25 @@
 
 # Report functions
+import csv
+
+
+def read_file(filename = "game_stat.txt"):  # reads an external file
+    '''
+    Argument: a filename
+    Returns: list
+    '''
+    with open(filename, newline = "\n") as file:
+        reader = csv.reader(file, delimiter = "\t")
+        data = []
+        for row in reader:
+            #row = [title, number_sold, release_date, genre, publisher]
+            title = row[0]
+            number_sold = float(row[1])
+            release_date = row[2]
+            genre = row[3]
+            publisher = row[4]
+            data.append([title, number_sold, release_date, genre, publisher])
+        return(data)
+
+
+def count_games(filename):
