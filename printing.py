937cbaeb81442b8e4aa245c28d8a58f8690ac6fe
Henrik Miszoglad <henrik.miszoglad@gmail.com>
2017-06-26 10:04:05 +0200

    added pprint

--- printing.py
+++ printing.py
@@ -1,10 +1,12 @@
 from reports import *
+import pprint
 # Printing functions
-print(count_games("game_stat.txt"))
-print(decide("game_stat.txt", "2009"))
-print(get_latest("game_stat.txt"))
-print(count_by_genre("game_stat.txt", "RPG"))
-print(get_line_number_by_title("game_stat.txt", "Command & Conquer"))
-print(sort_abc("game_stat.txt"))
-print(get_genres("game_stat.txt"))
-print(when_was_top_sold_fps("game_stat.txt"))
+pp = pprint.PrettyPrinter(width=80, compact=True)
+pp.pprint(count_games("game_stat.txt"))
+pp.pprint(decide("game_stat.txt", "2009"))
+pp.pprint(get_latest("game_stat.txt"))
+pp.pprint(count_by_genre("game_stat.txt", "RPG"))
+pp.pprint(get_line_number_by_title("game_stat.txt", "Command & Conquer"))
+pp.pprint(sort_abc("game_stat.txt"))
+pp.pprint(get_genres("game_stat.txt"))
+pp.pprint(when_was_top_sold_fps("game_stat.txt"))
