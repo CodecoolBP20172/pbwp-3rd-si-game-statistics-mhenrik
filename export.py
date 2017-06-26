728e569bef9b3b1a75729e5d96573fd2793d7097
Henrik Miszoglad <henrik.miszoglad@gmail.com>
2017-06-26 09:31:30 +0200

    created the export.py, output.py and printing.py. All tests work fine.

--- export.py
+++ export.py
@@ -1,2 +1,24 @@
+from reports import *
+# Export function
 
-# Export functions
+
+def export_all(file_name):
+    '''
+    Exports all answer to a .txt file
+    Args:
+        param1: a file name
+    Returns:
+        a text file
+    '''
+    with open(file_name, 'w', newline="\n") as file:
+        file.writelines(str(count_games("game_stat.txt"))+"\n")
+        file.writelines(str(decide("game_stat.txt", "2009"))+"\n")
+        file.writelines(str(get_latest("game_stat.txt"))+"\n")
+        file.writelines(str(count_by_genre("game_stat.txt", "RPG"))+"\n")
+        file.writelines(str(get_line_number_by_title("game_stat.txt", "Command & Conquer"))+"\n")
+        file.writelines(str(sort_abc("game_stat.txt"))+"\n")
+        file.writelines(str(get_genres("game_stat.txt"))+"\n")
+        file.writelines(str(when_was_top_sold_fps("game_stat.txt")))
+
+
+export_all("output.txt")
--- reports.py
+++ reports.py
@@ -3,38 +3,48 @@
 import csv
 import string
 
-def read_file(file_name, title = None):  # reads an external file
+
+def read_file(file_name, title=None):
     '''
-    Argument: a file name
-    Returns: a list
+    Reads an external file
+    Args:
+        param1: a file name
+    Returns:
+        a list
     '''
-    with open(file_name, newline = "\n") as file:
-        reader = csv.reader(file, delimiter = "\t")
+    with open(file_name, newline="\n") as file:
+        reader = csv.reader(file, delimiter="\t")
         data = []
         for row in reader:
-            #row = [title, number_sold, release_date, genre, publisher]
+            # row = [title, number_sold, release_date, genre, publisher]
             title = 0
             number_sold = 1
             release_date = 2
             genre = 3
             publisher = 4
-            data.append([row[title], float(row[number_sold]), row[release_date], row[genre], row[publisher]])
+            data.append([row[title], float(row[number_sold]),
+                        int(row[release_date]), row[genre], row[publisher]])
         return(data)
 
 
 def count_games(file_name):
     '''
-    Argument: a file name
     Counts the number of games in a file.
-    Returns: an integer
+    Args:
+        param1: a file name
+    Returns:
+        an integer
     '''
     return(len(read_file(file_name)))
 
 
 def decide(file_name, year):
     '''
-    Arguments: a file name and a year
-    Returns: a boolean if there is a game in the given year
+    Decides if there is a game from a given year
+    Args:
+        param1: a file name and a year
+    Returns:
+        a boolean if there is a game in the given year
     '''
     return any(year in game for game in read_file(file_name))
 
@@ -42,24 +52,29 @@ def decide(file_name, year):
 def get_latest(file_name):
     '''
     Finds the title of the latest game
-    Argument: a file name
-    Returns: the title of the game as a string
+    Args:
+        param1: a file name
+    Returns:
+        the title of the game as a string
     '''
-    #row = [title, number_sold, release_date, genre, publisher]
+    # row = [title, number_sold, release_date, genre, publisher]
     years = []
     title = []
     for i, game in enumerate(read_file(file_name)):
         years.append(read_file(file_name)[i][2])
         title.append(read_file(file_name)[i][0])
-    mix = list(zip(years,title))
-    return(sorted(mix, key = lambda x: x[0], reverse = True)[0][1])
+    mix = list(zip(years, title))
+    return(sorted(mix, key=lambda x: x[0], reverse=True)[0][1])
 
 
 def count_by_genre(file_name, genre):
     '''
     Counts the games by the given genre.
-    Arguments: a file name and a genre
-    Returns: an integer
+    Args:
+        param1: a file name
+        param2: a genre
+    Returns:
+        an integer
     '''
     genres = []
     for i, game in enumerate(read_file(file_name)):
@@ -67,7 +82,15 @@ def count_by_genre(file_name, genre):
     return(genres.count(genre))
 
 
-def line_number_by_title(file_name, title):
+def get_line_number_by_title(file_name, title):
+    '''
+    Finds the line number of the given game (by title)
+    Args:
+        param1: a file name
+        param2 : a title
+    Returns:
+        an integer
+    '''
     try:
         return([title in game for game in read_file(file_name)].index(True)+1)
     except BaseException:
@@ -75,27 +98,36 @@ def line_number_by_title(file_name, title):
 
 
 def sort_list(indexlist):
+    '''
+    Sorting algorithm.
+    Args:
+        param1: a list
+    Returs:
+        a sorted list
+    '''
     i = 1
     while i < len(indexlist):
         j = 0
-        while j <= len(indexlist) -2 :
+        while j <= len(indexlist) - 2:
             if indexlist[j] > indexlist[j+1]:
                 temp = indexlist[j+1]
                 indexlist[j+1] = indexlist[j]
                 indexlist[j] = temp
-                j = j + 1
+                j += 1
             else:
-                j = j+1
-        i = i +1
+                j += 1
+        i += 1
     return(indexlist)
+
 
 def sort_abc(file_name):
     '''
     Sorts the games by title.
-    Argument: a file name
-    Returns: a list of strings
+    Args:
+        param1: a file name
+    Returns:
+        a list of strings
     '''
-
     lower_list = list(string.printable)
     first_letters = []
     second_letter = []
@@ -122,26 +154,29 @@ def sort_abc(file_name):
     mix = list(zip(indexlist1, indexlist2, indexlist3, titles))
     a = sort_list(mix)
     b = [row[-1] for row in a]
-    print(b)
+    return(b)
 
-sort_abc("game_stat.txt")
 
 def get_genres(file_name):
     '''
     Find the genres in the file.
-    Argument: a file name
-    Returns: abc sorted list of the genres
+    Args:
+        param1: a file name
+    Returns:
+        abc sorted list of the genres
     '''
     genres = []
     for i, game in enumerate(read_file(file_name)):
         genres.append(read_file(file_name)[i][3])
-    return(sorted(set(genres)))
+    return(sorted(set(genres), key=str.lower))
 
 
 def when_was_top_sold_fps(file_name):
     '''Finds the release date of the top sold "First-person shooter" game
-    Argument: a file name
-    Returns: an integer
+    Args:
+    param1: a file name
+    Returns:
+        an integer
     '''
     sold = []
     date = []
@@ -150,7 +185,7 @@ def when_was_top_sold_fps(file_name):
             if read_file(file_name)[i][3] == "First-person shooter":
                 sold.append(read_file(file_name)[i][1])
                 date.append(read_file(file_name)[i][2])
-        mix = list(zip(sold,date))
-        return sorted(mix, key = lambda x: x[0], reverse = True)[0][1]
+        mix = list(zip(sold, date))
+        return int(sorted(mix, key=lambda x: x[0], reverse=True)[0][1])
     else:
         raise ValueError
